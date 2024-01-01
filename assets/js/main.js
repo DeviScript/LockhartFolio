de362f9f9c83211545398d8547f4657e22d314f8
bslockharts <brian.lockhart@outlook.com>
2024-01-01 12:49:01 -0500

    Updated portfolio with new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+  /** Easy selector helper function */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -13,9 +11,7 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
+  /** Easy event listener function */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -27,16 +23,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+  /** Easy on scroll event listener */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+  /** Navbar links active state on scroll */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -54,9 +46,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
+  /** Scrolls to an element with header offset */
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -72,9 +62,7 @@
     })
   }
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
+  /** Toggle .header-scrolled class to #header when page is scrolled */
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
@@ -88,9 +76,7 @@
     onscroll(document, headerScrolled)
   }
 
-  /**
-   * Back to top button
-   */
+  /** Back to top button */
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -104,18 +90,14 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
+  /** Mobile nav toggle */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Mobile nav dropdowns activate
-   */
+  /** Mobile nav dropdowns activate */
   on('click', '.navbar .dropdown > a', function(e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
@@ -123,9 +105,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on links with a class name .scrollto
-   */
+  /** Scroll with ofset on links with a class name .scrollto */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
@@ -141,9 +121,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
+  /** Scroll with ofset on page load with hash links in the url */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -152,9 +130,7 @@
     }
   });
 
-  /**
-   * Intro type effect
-   */
+  /** * Intro type effect */
   const typed = select('.typed')
   if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
@@ -168,16 +144,12 @@
     });
   }
 
-  /**
-   * Initiate portfolio lightbox 
-   */
+  /** Initiate portfolio lightbox  */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Preloader
-   */
+  /** Preloader */
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
